44bfa135b70b781313e0b4fd58f0f74a760d8677
yalanin <yalanin@gmail.com>
2023-09-06 22:42:55 +0800

    bean practice

---
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HpPrinter implements Printer{
+    @Override
+    public void print(String message) {
+        System.out.println("HP 印表機：" + message);
+    }
+}
