da38c65aef371d08b1fdce7546f760f1d5274f57
yalanin <yalanin@gmail.com>
2023-09-07 23:29:05 +0800

    autowire and qualifier practice

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
     @Autowired
+    @Qualifier("hpPrinter")
     private Printer printer;
 
     @RequestMapping("/test")
