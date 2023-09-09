8829e82aca45a452cbec28e8f41b1da9a05ceaac
yalanin <yalanin@gmail.com>
2023-09-10 01:16:06 +0800

    bean initialize practice

--- src/main/java/com/example/demo/CanonPrinter.java
+++ src/main/java/com/example/demo/CanonPrinter.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 //@Component
-public class CanonPrinter implements Printer {
+public class CanonPrinter implements Printer, InitializingBean {
+    private int count;
+
     @Override
     public void print(String message) {
+        count--;
         System.out.println("Canon 印表機：" + message);
+        System.out.println("剩餘使用次數：" + count);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        count = 20;
     }
 }
--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -2,10 +2,21 @@ package com.example.demo;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 //@Component
 public class HpPrinter implements Printer{
+    private int count;
+
+    @PostConstruct
+    public void initialize() {
+        count = 5;
+    }
+
     @Override
     public void print(String message) {
+        count--;
         System.out.println("HP 印表機：" + message);
+        System.out.println("剩餘使用次數：" + count);
     }
 }
