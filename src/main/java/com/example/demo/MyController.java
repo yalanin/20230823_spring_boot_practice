b8e681b782e8bc6bed6e7d9cf389f19ef7043f2d
yalanin <yalanin@gmail.com>
2023-11-04 14:41:27 +0800

    ResponseEntity practice

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +33,13 @@ public class MyController {
         list.add("橘子");
         store.setProductList(list);
         return store;
+    }
+
+    @RequestMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                "Hello World!"
+        );
     }
 
     @RequestMapping("/test1")
